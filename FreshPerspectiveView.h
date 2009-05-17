1ab0d844ea97f7c25bb3f3f00dc507804a5facd8
Mathew Chasan <aftermathew@gmail.com>
2009-05-17 12:52:43 -0700

    removed entire original program and restarted with a screensaver template.  Just ported screen saver template to ruby, worked without issue :-)

---
+++ FreshPerspectiveView.h
@@ -0,0 +1,17 @@
+//
+//  FreshPerspectiveView.h
+//  FreshPerspective
+//
+//  Created by Mathew Chasan on 5/17/09.
+//  Copyright (c) 2009, __MyCompanyName__. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+
+@interface FreshPerspectiveView : ScreenSaverView 
+{
+
+}
+
+@end
